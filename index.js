c921355b38f9d68715cd9a3c45aab56819fdc32b
joeyjojojunior <cmdrjoeyjojojunior@gmail.com>
2021-07-12 18:29:12 -0400

    more refactor and comment

--- index.js
+++ index.js
@@ -1,104 +1,49 @@
  var MAX_LABEL_CHARS = 14;
- var MAX_SL_PER_COL = 12;
  var NUM_COLS = 16;
  var NUM_ROWS = 8;
  var NUM_KNOBS = 128;
- var currKnob = 0;
  var knobs = new Array(NUM_KNOBS);
- var isWindowLoaded = false;
- var isFirstRun = true;
+ var drag = false;
 
- for (var i = 0; i < NUM_KNOBS; i++) {
-     gridDiv = document.getElementById("knobGrid");
+ function init() {
+     createGridItems();
+     createGrid();
+     createDivSettings();
+     showDivSettings("0");
 
-     gridItem = document.createElement("div");
-     gridItem.className = "item";
-     gridItem.id = `gridItem${i}`;
-     gridItem.addEventListener("mouseup", eventClickGridItem, false);
+     // Keeps grid item text in its container
+     jQuery(".knobContent").fitText(0.87);
 
-     gridItemContent = document.createElement("div");
-     gridItemContent.className = "item-content";
-     gridItemContent.id = `gridItemContent${i}`;
-
-     gridKnobContent = document.createElement("div");
-     gridKnobContent.className = "knobContent";
-     gridKnobContent.id = `knobContent${i}`;
-     gridKnobContent.innerHTML += `${i + 1}<br>THIS IS 14 CHR`
-
-     gridItemContent.appendChild(gridKnobContent);
-     gridItem.appendChild(gridItemContent);
-
-     knobs[i] = gridItem;
+     // Allows moving sub labels around
+     $(function () {
+         $(".listSublabels").sortable();
+         $(".listSublabels").disableSelection();
+     });
  }
 
- function createKnobDivSettings() {
-     var editDiv = document.querySelector(".editDiv");
+ // Create a div for each knob that contains all of its settings/sublabels
+ //   .class  divSettings
+ //   #id     divSettings${knobID}
+ function createDivSettings() {
+     var settingsDiv = document.querySelector(".settingsDiv");
 
      for (var i = 0; i < NUM_KNOBS; i++) {
-         var knobDivSettings = document.createElement("div");
-         knobDivSettings.className = "knobDivSettings";
-         knobDivSettings.id = `knobDivSettings${i}`;
-         knobDivSettings.style.display = "none";
-         knobDivSettings.appendChild(createKnobTableSettings(i));
-         editDiv.appendChild(knobDivSettings);
-         editDiv.appendChild(createKnobDivSublabels(i));
+         var divSettings = document.createElement("div");
+         divSettings.className = "divSettings";
+         divSettings.id = `divSettings${i}`;
+         divSettings.style.display = "none";
+         divSettings.appendChild(createTableSettings(i));
+         settingsDiv.appendChild(divSettings);
+         settingsDiv.appendChild(createDivSublabels(i));
      }
  }
 
- function createKnobDivPreset() {
-     //var preset = document.createElement("div")
- }
-
- function createKnobDivSublabels(knobID) {
-     var listDivSublabels = document.createElement("div");
-     listDivSublabels.className = "listDivSublabels";
-     listDivSublabels.id = `listDivSublabels${knobID}`;
-
-     var labelDiv = document.createElement("div");
-     labelDiv.className = "listDivSublabelsLabel";
-     labelDiv.innerHTML = "Sub Labels"
-
-     var listSublabels = document.createElement("ul");
-     listSublabels.className = "listSublabels";
-     listSublabels.id = `listSublabels${knobID}`;
-
-     var labels = new Array(Math.floor(Math.random() * (30 - 1 + 1)) + 1);
-     //var labels = new Array(128);
-
-     for (var i = 0; i < labels.length; i++) {
-         var subLabel = document.createElement("li");
-         subLabel.className = 'listSublabelItem'
-
-         var input = document.createElement("input");
-         input.className = "inputSubLabel";
-         input.id = `inputSubLabel${knobID}`
-         input.maxLength = MAX_LABEL_CHARS;
-         input.spellcheck = false;
-
-         var dragHandle = document.createElement("div");
-         dragHandle.className = "dragHandle";
-
-         var img = document.createElement("img");
-         img.src = "dragIcon.png";
-         img.className = "dragIcon";
-         dragHandle.appendChild(img);
-
-         subLabel.appendChild(dragHandle);
-         subLabel.appendChild(input);
-         listSublabels.appendChild(subLabel);
-
-     }
-
-     var test = labels.length / MAX_SL_PER_COL;
-     listSublabels.style.columnCount = Math.floor(labels.length / MAX_SL_PER_COL);
-
-     listDivSublabels.appendChild(labelDiv);
-     listDivSublabels.appendChild(listSublabels);
-     listDivSublabels.style.display = "none";
-     return listDivSublabels
- }
-
- function createKnobTableSettings(knobID) {
+ // Create a table that contains all the settings for a single knob
+ //   knobID  knob index
+ //   return  table  
+ //   .class  N/A
+ //   #id     N/A
+ function createTableSettings(knobID) {
      var tbl = document.createElement("table");
 
      //var thead = document.createElement("thead");
@@ -113,15 +58,17 @@
 
      var tbdy = document.createElement("tbody");
      var inputLabels = ["Label", "Channel", "CC", "Init Value", "Max Values", "Max Range", "Locked"];
-     var knobInputFields = createKnobInputFields(knobID);
 
+     // Create the input fields for this knob
+     var knobInputFields = createInputSettings(knobID);
+
+     // Add the labels/fields to the table
      for (var i = 0; i < inputLabels.length; i++) {
          var tr = document.createElement("tr");
 
          var tdInputLabel = document.createElement("td");
          tdInputLabel.innerHTML = inputLabels[i];
          if (inputLabels[i] === "Locked") tdInputLabel.className = "tdLocked";
-
 
          var tdInputField = document.createElement("td");
          tdInputField.appendChild(knobInputFields[i]);
@@ -136,7 +83,18 @@
      return tbl;
  }
 
- function createKnobInputFields(knobID) {
+ // Create all the input elements in the divSettings div
+ //   knobID  knob index
+ //   return  array of input fields and a checkbox  
+ //   .class  inputField
+ //   #id     inputLabel${knobID}      
+ //           inputChannel{$knobID}    
+ //           inputCC${knobID}        
+ //           inputInitValue${knobID}
+ //           inputMaxValues${knobID}
+ //           inputMaxRange${knobID}
+ //           inputIsLocked${knobID}
+ function createInputSettings(knobID) {
      var label = document.createElement("input");
      var channel = document.createElement("input");
      var cc = document.createElement("input");
@@ -215,134 +173,222 @@
      return inputs;
  }
 
+ function createDivPreset() {
+     //var preset = document.createElement("div")
+ }
+
+ // Create a div that contains all the sublabels for a single knob
+ //   knobID  knob index
+ //   .class  divSublabels
+ //   #id     divSublabels${knobID}
+ function createDivSublabels(knobID) {
+     // Create the main .divSublabels div
+     var divSublabels = document.createElement("div");
+     divSublabels.className = "divSublabels";
+     divSublabels.id = `divSublabels${knobID}`;
+
+     // Create the label for the sublabels section
+     var labelDiv = document.createElement("div");
+     labelDiv.className = "divSublabelsLabel";
+     labelDiv.innerHTML = "Sub Labels"
+
+     // Create the un-ordererd list for the sublabels
+     var listSublabels = document.createElement("ul");
+     listSublabels.className = "listSublabels";
+     listSublabels.id = `listSublabels${knobID}`;
+
+     // Create test labels
+     var labels = new Array(Math.floor(Math.random() * (30 - 1 + 1)) + 1);
+     //var labels = new Array(128);
+     for (var i = 0; i < labels.length; i++) {
+         var subLabel = document.createElement("li");
+         subLabel.className = 'listSublabelItem'
+
+         var input = document.createElement("input");
+         input.className = "inputSubLabel";
+         input.id = `inputSubLabel${knobID}`
+         input.maxLength = MAX_LABEL_CHARS;
+         input.spellcheck = false;
+
+         var dragHandle = document.createElement("div");
+         dragHandle.className = "dragHandle";
+
+         var img = document.createElement("img");
+         img.src = "./img/dragIcon.png";
+         img.className = "dragIcon";
+         dragHandle.appendChild(img);
+
+         subLabel.appendChild(dragHandle);
+         subLabel.appendChild(input);
+         listSublabels.appendChild(subLabel);
+     }
+
+     // Hide the div and return in
+     divSublabels.appendChild(labelDiv);
+     divSublabels.appendChild(listSublabels);
+     divSublabels.style.display = "none";
+
+     return divSublabels
+ }
+
+ // Event handler for clicking on a grid item. Shows the settings for
+ // a knob by unhiding its divSettings div and hiding all the others
+ //
+ // TODO: No reason to hide EVERY knob after init since only one
+ // will be visible at a time. Modify to keep track of the current knob
+ // and only hide that one.
  function eventClickGridItem(e) {
-     var editDiv = document.querySelector(".editDiv");
+     var settingsDiv = document.querySelector(".settingsDiv");
      var knobIDString = e.target.id;
      var knobID = (knobIDString.match(/\d+/g) || []).map(n => parseInt(n))[0];
-     var knobDivSettings = document.getElementById(`knobDivSettings${knobID}`);
-     var listDivSublabels = document.getElementById(`listDivSublabels${knobID}`);
+     var divSettings = document.getElementById(`divSettings${knobID}`);
+     var divSublabels = document.getElementById(`divSublabels${knobID}`);
 
      var sl = document.getElementById(`listSublabels${knobID}`);
 
      if (!drag) {
-         hideKnobDivSettingsAll();
-         knobDivSettings.style.display = "block";
-         listDivSublabels.style.display = "block";
-     }
-     currKnob = knobID;
- }
-
- function showKnobDivSettings(knobID) {
-     var knobDivSettings = document.getElementById(`knobDivSettings${knobID}`);
-     var listDivSublabels = document.getElementById(`listDivSublabels${knobID}`);
-
-     knobDivSettings.style.display = "block";
-     listDivSublabels.style.display = "block";
- }
-
- function hideKnobDivSettingsAll() {
-     var knobDivSettingsAll = document.getElementsByClassName("knobDivSettings");
-     var listDivSublabelsAll = document.getElementsByClassName("listDivSublabels");
-     for (var i = 0; i < knobDivSettingsAll.length; i++) {
-         knobDivSettingsAll[i].style.display = "none";
-         listDivSublabelsAll[i].style.display = "none";
+         hideDivSettingsAll();
+         divSettings.style.display = "block";
+         divSublabels.style.display = "block";
      }
  }
 
- var grid = new Muuri('.grid', {
-     items: knobs,
-     dragEnabled: true,
-     dragSortHeuristics: {
-         sortInterval: 200,
-         minDragDistance: 20,
-         minBounceBackAngle: 1
-     },
-     dragSortPredicate: {
-         threshold: 50,
-         action: 'swap',
-         migrateAction: 'move'
-     },
-     layout: function (grid, layoutId, items, width, height, callback) {
-         var layout = {
-             id: layoutId,
-             items: items,
-             slots: [],
-             styles: {},
-         };
+ // Unhides a divSettings div for a knob by setting display to "block"
+ function showDivSettings(knobID) {
+     var divSettings = document.getElementById(`divSettings${knobID}`);
+     var divSublabels = document.getElementById(`divSublabels${knobID}`);
 
-         // Calculate the slots asynchronously. Note that the timeout is here only
-         // as an example and does not help at all in the calculations. You should
-         // offload the calculations to web workers if you want real benefits.
-         // Also note that doing asynchronous layout is completely optional and you
-         // can call the callback function synchronously also.
-         var timerId = window.setTimeout(function () {
-             var item,
-                 m,
-                 x = 0,
-                 y = 0,
-                 w = 0,
-                 h = 0;
+     divSettings.style.display = "block";
+     divSublabels.style.display = "block";
+ }
 
-             for (var i = 0; i < items.length; i++) {
-                 item = items[i];
-                 x += w;
-                 if (i % NUM_COLS == 0) {
-                     x = 0;
-                     y += h;
+ // Hides all the divSettings divs by setting display to "none"
+ function hideDivSettingsAll() {
+     var divSettingsAll = document.getElementsByClassName("divSettings");
+     var divSublabelsAll = document.getElementsByClassName("divSublabels");
+     for (var i = 0; i < divSettingsAll.length; i++) {
+         divSettingsAll[i].style.display = "none";
+         divSublabelsAll[i].style.display = "none";
+     }
+ }
+
+ // Creates all the Muuri grid items for the knobs
+ function createGridItems() {
+     for (var i = 0; i < NUM_KNOBS; i++) {
+         gridDiv = document.getElementById("knobGrid");
+
+         gridItem = document.createElement("div");
+         gridItem.className = "item";
+         gridItem.id = `gridItem${i}`;
+         gridItem.addEventListener("mouseup", eventClickGridItem, false);
+
+         gridItemContent = document.createElement("div");
+         gridItemContent.className = "item-content";
+         gridItemContent.id = `gridItemContent${i}`;
+
+         gridKnobContent = document.createElement("div");
+         gridKnobContent.className = "knobContent";
+         gridKnobContent.id = `knobContent${i}`;
+         gridKnobContent.innerHTML += `${i + 1}<br>THIS IS 14 CHR`
+
+         gridItemContent.appendChild(gridKnobContent);
+         gridItem.appendChild(gridItemContent);
+
+         knobs[i] = gridItem;
+     }
+ }
+
+ // Creates the Muuri grid
+ function createGrid() {
+     var grid = new Muuri('.grid', {
+         items: knobs,
+         dragEnabled: true,
+         dragSortHeuristics: {
+             sortInterval: 200,
+             minDragDistance: 20,
+             minBounceBackAngle: 1
+         },
+         dragSortPredicate: {
+             threshold: 50,
+             action: 'swap',
+             migrateAction: 'move'
+         },
+         layout: function (grid, layoutId, items, width, height, callback) {
+             var layout = {
+                 id: layoutId,
+                 items: items,
+                 slots: [],
+                 styles: {},
+             };
+
+             var timerId = window.setTimeout(function () {
+                 var item, m, x = 0,
+                     y = 0,
+                     w = 0,
+                     h = 0;
+
+                 for (var i = 0; i < items.length; i++) {
+                     item = items[i];
+                     x += w;
+                     if (i % NUM_COLS == 0) {
+                         x = 0;
+                         y += h;
+                     }
+                     m = item.getMargin();
+                     w = item.getWidth() + m.left +
+                         m.right;
+                     h = item.getHeight() + m.top + m.bottom;
+                     layout.slots.push(x, y);
                  }
-                 m = item.getMargin();
-                 w = item.getWidth() + m.left +
-                     m.right;
-                 h = item.getHeight() + m.top + m.bottom;
-                 layout.slots.push(x, y);
-             }
-             w += x;
-             h += y;
 
-             // Set the CSS styles that should be applied to the grid element.
-             layout.styles.width = w + 'px';
-             layout.styles.height = h + 'px';
+                 w += x;
+                 h += y;
 
-             // When the layout is fully computed let 's call the callback function and 
-             // provide the layout object as it's argument.
-             callback(layout);
-         }, 200);
+                 // Set the CSS styles that should be applied to the grid element.
+                 layout.styles.width = w + 'px';
+                 layout.styles.height = h + 'px';
 
-         // If you are doing an async layout you _can_ (if you want to) return a 
-         // function that cancels this specific layout calculations if it 's still 
-         // processing/queueing when the next layout is requested.
-         return function () {
-             window.clearTimeout(timerId);
-         };
-     },
- });
+                 // When the layout is fully computed let 's call the callback function and 
+                 // provide the layout object as it's argument.
+                 callback(layout);
+             }, 200);
 
- let drag = false;
+             // If you are doing an async layout you _can_ (if you want to) return a 
+             // function that cancels this specific layout calculations if it 's still 
+             // processing/queueing when the next layout is requested.
+             return function () {
+                 window.clearTimeout(timerId);
+             };
+         },
+     });
+ }
 
- document.addEventListener(
-     'mousedown', () => drag = false);
+ // Disables zoom by disabling the Ctrl key
+ function zoomDisable(e) {
+     if (e.ctrlKey) {
+         return false;
+     }
+ }
 
- document.addEventListener(
-     'mousemove', () => drag = true);
 
- document.addEventListener(
-     'mouseup', () => drag ? 'drag' : 'click');
+ // Sets the drag flag which is used to prevent firing click 
+ // events at the start and end of dragging actions
+ window.onload = () => {
+     document.addEventListener(
+         'mousedown', () => drag = false);
 
- createKnobDivSettings();
- showKnobDivSettings("0");
+     document.addEventListener(
+         'mousemove', () => drag = true);
 
- // Keeps grid item text in its container
- jQuery(".knobContent").fitText(0.87);
+     document.addEventListener(
+         'mouseup', () => drag ? 'drag' : 'click');
 
- // Allows moving sub labels around
- $(function () {
-     $(".listSublabels").sortable();
-     $(".listSublabels").disableSelection();
- });
+     //window.addEventListener('resize', eventZoomChanged);
+     // window.onkeydown = zoomDisable;
+ }
 
- window.addEventListener('load', (event) => {});
+ init();
 
- //window.addEventListener('resize', eventZoomChanged);
 
  /*
     var mainDiv = document.querySelector(".mainDiv");
@@ -363,11 +409,3 @@
     mainDiv.appendChild(b2);
     mainDiv.appendChild(b3);
 */
-
- // window.onkeydown = zoomDisable;
-
- function zoomDisable(e) {
-     if (e.ctrlKey) {
-         return false;
-     }
- }
